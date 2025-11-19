9716244695c9a145f5997193db1d3ab6833ec71e
Kael Mog <kaelmogot@gmail.com>
2025-11-19 10:14:39 +0700

    I am going insane

--- components/ReportForm.jsx
+++ components/ReportForm.jsx
@@ -194,8 +194,6 @@ export default function ReportForm({ existingReport, mode = 'create' }) {
     }
   };
 
-  console.log('CL REP ==>', form);
-
   return (
     <div className="min-h-screen px-4 py-6 max-w-md mx-auto bg-primary">
       <header className="mb-4 animate-fade-in flex space-between">
